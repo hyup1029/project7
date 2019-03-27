f5de41db7013fc22f17f9e3f3af5ec6c0e84fb4b
sang <psh2678@hanmir.com>
2019-03-27 11:06:39 +0900

    Test 추가

---
+++ project7/src/project7/MemberTest.java
@@ -0,0 +1,22 @@
+package project7;
+
+public class MemberTest {
+	private int number;
+	private int phone_number;
+	public int getNumber() {
+		return number;
+	}
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	public int getPhone_number() {
+		return phone_number;
+	}
+	public void setPhone_number(int phone_number) {
+		this.phone_number = phone_number;
+	}
+	
+	
+	
+	
+}
